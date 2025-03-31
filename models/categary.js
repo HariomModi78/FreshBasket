16f28f3cc997689ca33e07462bef0b39bcb3295b
Hariom Modi <hariommodi7898@gmail.com>
2025-03-31 17:04:18 +0530

    Delete models directory

--- models/categary.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categarySchema = mongoose.Schema({
-    productId:Array,
-    name:String,
-    imagePath:String
-})
-
-module.exports = mongoose.model("categary",categarySchema);
