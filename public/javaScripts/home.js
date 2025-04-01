77f0356d59ff7e24599ff7309e45bb5de3c83cf6
Hariom Modi <hariommodi7898@gmail.com>
2025-04-01 20:59:43 +0530

    Update home.js

--- public/javaScripts/home.js
+++ public/javaScripts/home.js
@@ -2,7 +2,36 @@ socket = io();
 let item = document.getElementsByClassName("item");
 let categary = document.getElementsByClassName("categary");
 let input = document.querySelector(".input");
+let main = document.querySelector("main");
 
+let y=0;
+window.addEventListener("scroll",function(event){
+    if(window.scrollY>0 && window.scrollY>y){
+        home.style.cssText = "animation:move linear forwards 1s;"
+        feed.style.cssText = "animation:move linear forwards 1s;"
+        cart.style.cssText = "animation:move linear forwards 1s;"
+        seller.style.cssText = "animation:move linear forwards 1s;"
+        more.style.cssText = "animation:move linear forwards 1s;"
+        setTimeout(function(){
+            y = window.scrollY;
+        },300);
+        console.log("y = ",y);
+    }
+    else{
+        home.style.cssText = "animation:move1 linear forwards 1s;"
+        feed.style.cssText = "animation:move1 linear forwards 1s;"
+        cart.style.cssText = "animation:move1 linear forwards 1s;"
+        seller.style.cssText = "animation:move1 linear forwards 1s;"
+        more.style.cssText = "animation:move1 linear forwards 1s;"
+        setTimeout(function(){
+            y = window.scrollY;
+        },300);
+        
+
+    }
+console.log(window.scrollX)
+console.log(window.scrollY)
+})
 for(let i=0;i<item.length;i++){
     item[i].addEventListener("click",function(){
         window.location.href = `/productView/${item[i].id}`
