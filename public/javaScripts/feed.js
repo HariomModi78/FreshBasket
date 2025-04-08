37bdf701a85710af02047b18547c04608b2abc34
Hariom Modi <hariommodi7898@gmail.com>
2025-04-08 20:27:19 +0530

    Add files via upload

---
+++ public/javaScripts/feed.js
@@ -0,0 +1,6 @@
+let item = document.getElementsByClassName("item");
+for(let i=0;i<item.length;i++){
+    item[i].addEventListener("click",function(){
+        window.location.href = `/productView/${item[i].id}`
+    })
+}
