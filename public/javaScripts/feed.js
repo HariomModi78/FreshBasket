8bab841e6a324b22478a6a1260bbf178a947dd31
Hariom Modi <hariommodi7898@gmail.com>
2025-04-11 13:09:09 +0530

    Delete public directory

--- public/javaScripts/feed.js
+++
@@ -1,6 +0,0 @@
-let item = document.getElementsByClassName("item");
-for(let i=0;i<item.length;i++){
-    item[i].addEventListener("click",function(){
-        window.location.href = `/productView/${item[i].id}`
-    })
-}
--- public/javaScripts/orderStatus.js
+++
@@ -1,6 +0,0 @@
-// console.log(new Date().getTime());
-
-let cancel = document.querySelector(".cancel");
-cancel.addEventListener("click",function(){
-    window.location.href = `/cancelOrder/${cancel.id}`
-})
