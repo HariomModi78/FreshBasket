7232548e33f68ec8c03900290c21c2a9f2104760
Hariom Modi <hariommodi7898@gmail.com>
2025-04-12 23:25:55 +0530

    Delete public directory

--- public/javaScripts/index.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(reg => console.log("Service Worker Registered"))
-      .catch(err => console.error("Service Worker registration failed:", err));
-  }
-  
