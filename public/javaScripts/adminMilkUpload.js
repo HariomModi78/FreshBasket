27f6fcb825a1e2bda05bbf1a8461a064588eb261
Hariom Modi <hariommodi7898@gmail.com>
2025-04-13 16:25:03 +0530

    Add files via upload

---
+++ public/javaScripts/adminMilkUpload.js
@@ -0,0 +1,9 @@
+let user = document.getElementsByClassName("user");
+
+for(let i=0;i<user.length;i++){
+    user[i].addEventListener("click",function(event){
+    console.log(event.target.id)
+
+        window.location.href = `/admin/milkUpload/${user[i].id}`
+    }) 
+}
