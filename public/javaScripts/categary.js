76f916dc1ac675a7d8680e66af3257ecabc20ecd
Hariom Modi <hariommodi7898@gmail.com>
2025-04-15 11:58:33 +0530

    Add files via upload

---
+++ public/javaScripts/categary.js
@@ -0,0 +1,17 @@
+let item = document.getElementsByClassName("item");
+for(let i=0;i<item.length;i++){
+    item[i].addEventListener("click",function(){
+        window.location.href = `/productView/${item[i].id}`
+    })
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+});
+
+let input = document.querySelector(".input");
+
+input.addEventListener("click",function(){
+    window.location.href = "/search"
+})
