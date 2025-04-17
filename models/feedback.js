42d46fbe6d7d3a83ea944a1bdbed483cfca03440
Hariom Modi <hariommodi7898@gmail.com>
2025-04-17 16:11:45 +0530

    Delete models directory

--- models/feedback.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const feedbackSchema = mongoose.Schema({
-    userId:String,
-    feedback:String,
-
-})
-
-module.exports = mongoose.model("feedback",feedbackSchema);
