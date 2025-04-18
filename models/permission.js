68a390e678da13765ca8048a007954b4c1e69ca6
Hariom Modi <hariommodi7898@gmail.com>
2025-04-18 23:21:29 +0530

    Delete models directory

--- models/permission.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const permissionSchema = mongoose.Schema({
-    userId:String,
-    type:String
-})
-
-module.exports = mongoose.model("permission",permissionSchema);
