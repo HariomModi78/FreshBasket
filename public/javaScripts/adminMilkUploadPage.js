f12305cc1bd1c436b6ea8303c61400efaa60bd11
Hariom Modi <hariommodi7898@gmail.com>
2025-05-01 10:13:08 +0530

    Delete public directory

--- public/javaScripts/adminMilkUploadPage.js
+++
@@ -1,5 +0,0 @@
-let time = document.querySelector("select");
-let timeInput = document.querySelector(".timeInput")
-time.addEventListener("input",function(){
-    timeInput.value = time.value;
-})
--- public/javaScripts/adminPermissionPage.js
+++
@@ -1,9 +0,0 @@
-let approve = document.querySelector(".approve");
-let reject = document.querySelector(".reject");
-
-approve.addEventListener("click",function(){
-    window.location.href = `/permission/approve/${approve.id}`
-})
-reject.addEventListener("click",function(){
-    window.location.href = `/permission/reject/${approve.id}`
-})
